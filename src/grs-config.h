6f316a1569b0ce9c9746394eda1b7ac08bf0e729
Gruve_p <gruve-p@users.noreply.github.com>
2018-09-20 20:12:27 +0200

    Bump version

--- src/grs-config.h
+++ src/grs-config.h
@@ -8,14 +8,14 @@
 
 #define CLIENT_VERSION_MAJOR 2
 #define CLIENT_VERSION_MINOR 16
-#define CLIENT_VERSION_REVISION 0
-#define CLIENT_VERSION_BUILD 1
+#define CLIENT_VERSION_REVISION 3
+#define CLIENT_VERSION_BUILD 0
 
 #define CLIENT_VERSION_IS_RELEASE true
 
 #define COPYRIGHT_YEAR 2018
 
-#define BUILD_DESC "v2.16.0.1"
+#define BUILD_DESC "v2.16.3"
 
 #define COPYRIGHT_HOLDERS "The %s developers"
 #define COPYRIGHT_HOLDERS_FINAL "The Groestlcoin Core developers"
