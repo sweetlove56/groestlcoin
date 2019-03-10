714d5d2d6ce0bf477bd27646d11ca59cde2e37ce
Gruve_p <gruve-p@users.noreply.github.com>
2019-03-10 13:41:32 +0100

    Minor fixes for linearize

--- contrib/linearize/linearize-data.py
+++ contrib/linearize/linearize-data.py
@@ -282,9 +282,9 @@ if __name__ == '__main__':
     settings['rev_hash_bytes'] = settings['rev_hash_bytes'].lower()
 
     if 'netmagic' not in settings:
-        settings['netmagic'] = 'f9beb4d9'
+        settings['netmagic'] = 'f9beb4d4'
     if 'genesis' not in settings:
-        settings['genesis'] = '000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f'
+        settings['genesis'] = '00000ac5927c594d49cc0bdb81759d0da8297eb614683d3acb62f0703b639023'
     if 'input' not in settings:
         settings['input'] = 'input'
     if 'hashlist' not in settings:
