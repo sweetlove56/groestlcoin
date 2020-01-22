d6b52a7758d50c4ec57c284bd86510c01d008c49
jackielove4u <jackielove4u@hotmail.com>
2020-01-22 18:24:54 +0100

    Correct organisation name

--- src/qt/guiconstants.h
+++ src/qt/guiconstants.h
@@ -46,7 +46,7 @@ static const int MAX_URI_LENGTH = 255;
 /* Number of frames in spinner animation */
 #define SPINNER_FRAMES 36
 
-#define QAPP_ORG_NAME "Groestkcoin"
+#define QAPP_ORG_NAME "Groestlcoin"
 #define QAPP_ORG_DOMAIN "groestlcoin.org"
 #define QAPP_APP_NAME_DEFAULT "Groestlcoin-Qt"
 #define QAPP_APP_NAME_TESTNET "Groestlcoin-Qt-testnet"
