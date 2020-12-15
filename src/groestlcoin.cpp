225f5fe0611dc70bf3215a56b433b38fe0d00de2
jackielove4u <jackielove4u@hotmail.com>
2020-12-15 22:24:08 +0100

    Add signet onion v3 seed

--- src/groestlcoin.cpp
+++ src/groestlcoin.cpp
@@ -512,7 +512,9 @@ public:
 
         if (!args.IsArgSet("-signetchallenge")) {
             bin = ParseHex("512103ad5e0edad18cb1f0fc0d28a3d4f1f3e445640337489abb10404f2d1e086be430210359ef5021964fe22d6f8e05b2463c9540ce96883fe3b278760f048f5189f2e6c452ae");
-            vSeeds.emplace_back("signet.groestlcoin.org");
+            vSeeds.emplace_back("198.199.105.43");
+            vSeeds.emplace_back("2604:a880:1:20::96:6001");
+            vSeeds.emplace_back("ubmgcth2ngfb7qapyrkpnn3i6p2dmu76zvd3hfs2mw3u4t54v2qa66id.onion:31331");
 
             consensus.nMinimumChainWork = uint256{};
             consensus.defaultAssumeValid = uint256{};
