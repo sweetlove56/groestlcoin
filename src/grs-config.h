9646a70826b494a6fd3236a59a627d3a927813c7
gruve-p <jackielove4u@hotmail.com>
2021-04-16 19:43:49 +0200

    Bump version

--- src/grs-config.h
+++ src/grs-config.h
@@ -8,14 +8,14 @@
 
 #define CLIENT_VERSION_MAJOR 2
 #define CLIENT_VERSION_MINOR 21
-#define CLIENT_VERSION_REVISION 0
+#define CLIENT_VERSION_REVISION 1
 #define CLIENT_VERSION_BUILD 0
 
 #define CLIENT_VERSION_IS_RELEASE true
 
-#define COPYRIGHT_YEAR 2020
+#define COPYRIGHT_YEAR 2021
 
-#define BUILD_DESC "v2.21.0"
+#define BUILD_DESC "v2.21.1"
 
 #define COPYRIGHT_HOLDERS "The %s developers"
 #define COPYRIGHT_HOLDERS_FINAL "The Groestlcoin Core developers"
