82fd4225dab661e787f9c1000fcedfb19c079f23
gruve-p <jackielove4u@hotmail.com>
2022-02-22 12:14:55 +0100

    comment default_max_tries

--- src/rpc/mining.h
+++ src/rpc/mining.h
@@ -6,6 +6,6 @@
 #define BITCOIN_RPC_MINING_H
 
 /** Default max iterations to try in RPC generatetodescriptor, generatetoaddress, and generateblock. */
-static const uint64_t DEFAULT_MAX_TRIES{100000000};
+static const uint64_t DEFAULT_MAX_TRIES{100000000}; // GRS
 
 #endif // BITCOIN_RPC_MINING_H
